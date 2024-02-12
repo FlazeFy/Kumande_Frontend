343dfc888f7ea1b8dc416052fea6901e045a2e37
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-02-12 14:08:15 +0700

    get most consume by provide pie chart

--- src/pages/stats/index.js
+++ src/pages/stats/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import GetNavbar from "../../components/bars/navbar"
 import GetNavbarToggle from "../../components/bars/navbartoggle"
 import GetMostConsumeFrom from "./usecases/get_most_consume_from"
+import GetMostConsumeProvide from "./usecases/get_most_consume_provide"
 import GetMostConsumeType from "./usecases/get_most_consume_type"
 
 const StatsPage = () => {
@@ -18,6 +19,9 @@ const StatsPage = () => {
                         </div>
                         <div className="col-lg-4 col-md-4 col-sm-6">
                             <GetMostConsumeFrom ctx="most consume from"/>
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-6">
+                            <GetMostConsumeProvide ctx="most consume provide"/>
                         </div>
                     </div>
                 </div>
