e601c20284ca8c5b581751e8a38aeeeb5bb758bc
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-02-12 14:16:44 +0700

    get most consume by main ingredient pie chart

--- src/pages/stats/index.js
+++ src/pages/stats/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import GetNavbar from "../../components/bars/navbar"
 import GetNavbarToggle from "../../components/bars/navbartoggle"
 import GetMostConsumeFrom from "./usecases/get_most_consume_from"
+import GetMostConsumeMainIng from "./usecases/get_most_consume_main_ing"
 import GetMostConsumeProvide from "./usecases/get_most_consume_provide"
 import GetMostConsumeType from "./usecases/get_most_consume_type"
 
@@ -14,14 +15,17 @@ const StatsPage = () => {
                 <GetNavbarToggle/>
                 <div id="content-body">
                     <div className="row">
-                        <div className="col-lg-4 col-md-4 col-sm-6">
+                        <div className="col-lg-4 col-md-4 col-sm-6 py-3">
                             <GetMostConsumeType ctx="most consume type"/>
                         </div>
-                        <div className="col-lg-4 col-md-4 col-sm-6">
+                        <div className="col-lg-4 col-md-4 col-sm-6 py-3">
                             <GetMostConsumeFrom ctx="most consume from"/>
                         </div>
-                        <div className="col-lg-4 col-md-4 col-sm-6">
+                        <div className="col-lg-4 col-md-4 col-sm-6 py-3">
                             <GetMostConsumeProvide ctx="most consume provide"/>
+                        </div>
+                        <div className="col-lg-4 col-md-4 col-sm-6 py-3">
+                            <GetMostConsumeMainIng ctx="most consume main ingredient"/>
                         </div>
                     </div>
                 </div>
